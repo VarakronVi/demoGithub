3575a047c073a6205459b013ed12aae71f1ff522
VarakronVi <130995880+VarakronVi@users.noreply.github.com>
2024-06-23 10:04:05 +0700

    firstCommit

---
+++ ProjectGit/ProjectGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectGit
+//
+//  Created by hok191 on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectGit/ProjectGit/ProjectGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGitApp.swift
+//  ProjectGit
+//
+//  Created by hok191 on 23/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
